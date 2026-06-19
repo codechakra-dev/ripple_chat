3f3ef62001f9f3ca0e43de2dbda88fe8c8048e2a
akshayoffice244 <akshayoffice244@gmail.com>
2026-06-19 17:39:24 +0530

    forgot password method implemented

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,36 +1,51 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:ripple/core/constants/app_strings.dart';
 import 'package:ripple/core/utils/auth_error_helper.dart';
 import 'package:ripple/core/utils/email_validation.dart';
 import 'package:ripple/core/utils/snackbar_helper.dart';
 import 'package:ripple/firebase/auth_service.dart';
+import 'package:ripple/main.dart';
 
-class AuthProvider extends ChangeNotifier {
+class AuthenticationProvider extends ChangeNotifier {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final confirmPasswordController = TextEditingController();
   final passwordResetCodeController = TextEditingController();
+
+  //final resetCodeController = TextEditingController();
   var showPassword = false;
   var isLoading = false;
+
+  var isCodeSent = false;
+  var hidePassword = false;
   User? _firebaseUser;
   final AuthService _authService = AuthService();
 
   //Call this method in splash screen
-  void onInitialization({
-    required VoidCallback navigateToHomeScreen,
-    required VoidCallback navigateToLoginScreen,
-  }) {
+  void onInitialization() {
     // _firebaseUser = _authService.firebaseAuth.currentUser;
     _authService.firebaseAuth.authStateChanges().listen((user) {
       _firebaseUser = user;
       if (user == null) {
-        navigateToLoginScreen();
+        navigatorKey.currentState?.pushReplacementNamed(AppStrings.loginScreen);
       } else {
-        navigateToHomeScreen();
+        clearAllControllers();
+        navigatorKey.currentState?.pushReplacementNamed(AppStrings.homeScreen);
       }
       notifyListeners();
     });
+
+    emailController.addListener(() {
+      print("Email: ${emailController.text}");
+    });
+  }
+
+  void togglePasswordVisibility() {
+    hidePassword = !hidePassword;
+    print("Hide password: ${hidePassword}");
+    notifyListeners();
   }
 
   //Same method for sign in and sign up
@@ -172,10 +187,97 @@ class AuthProvider extends ChangeNotifier {
   //have two fields
   // one for email and other for reset code
 
-  Future<void> sendForgotPasswordEmail(String email) async {
+  Future<void> sendForgotPasswordEmail(
+    BuildContext context,
+    String email,
+  ) async {
+    if (email.isEmpty || !EmailValidation.isValidEmail(email)) {
+      SnackBarHelper.showSnackBar(
+        context,
+        "Invalid email",
+        "Please, enter a valid email!",
+      );
+      return;
+    }
+    try {
+      isCodeSent =false;
+      isLoading = true;
+      notifyListeners();
+     await _authService.forgotPassword(email);
+      isCodeSent = true;
+      notifyListeners();
+    } on FirebaseAuthException catch(e){
+      String message = Autherrorhelper.authErrorMessage(e.code);
+      if(context.mounted){
+        SnackBarHelper.showSnackBar(context, "Failed", message);
+      }
+    }catch (e) {
+      isCodeSent = false;
+      notifyListeners();
+      if (context.mounted) {
+        SnackBarHelper.showSnackBar(
+          context,
+          'Error',
+          'An unexpected error occurred: $e',
+        );
+      }
+    }finally{
+      isLoading  = false;
+      notifyListeners();
+    }
+  }
 
 
+  Future<void> confirmPasswordResetRequest(BuildContext context, String code, String newPassword) async {
 
+
+    if (newPassword.length < 6) {
+      SnackBarHelper.showSnackBar(
+        context,
+        "Weak Password",
+        "Password must be at least 6 characters long!",
+      );
+      return;
+    }
+
+    if (code.isEmpty) {
+      SnackBarHelper.showSnackBar(
+        context,
+        "Wrong Code",
+        "Please enter valid code!",
+      );
+      return;
+    }
+    try{
+        isLoading = true;
+
+        notifyListeners();
+      await _authService.confirmResetPassword(code, newPassword);
+      if(context.mounted){
+        SnackBarHelper.showSnackBar(context, "Success", "Password reset successfully!");
+      }
+    }on FirebaseAuthException catch(e){
+      String message = Autherrorhelper.authErrorMessage(e.code);
+      if(context.mounted){
+        SnackBarHelper.showSnackBar(context, "Failed", message);
+      }
+    }
+
+    catch(e){
+      if(context.mounted){
+        SnackBarHelper.showSnackBar(context, "Unexpected error: ", "$e");
+      }
+    }finally{
+      isLoading = false;
+      notifyListeners();
+    }
+
+  }
+  void clearAllControllers() {
+    emailController.clear();
+    passwordController.clear();
+    confirmPasswordController.clear();
+    passwordResetCodeController.clear();
   }
 
   @override
@@ -183,6 +285,7 @@ class AuthProvider extends ChangeNotifier {
     emailController.dispose();
     passwordController.dispose();
     confirmPasswordController.dispose();
+    passwordResetCodeController.dispose();
     super.dispose();
   }
 }

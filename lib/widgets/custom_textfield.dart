7d3b06a98624b2d26a270e9724b61918c564ebb8
akshayoffice244 <akshayoffice244@gmail.com>
2026-06-19 17:37:57 +0530

    custom text field created

--- lib/widgets/custom_textfield.dart
+++ lib/widgets/custom_textfield.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:ripple/core/constants/app_strings.dart';
 
-class CustomTextfield extends StatelessWidget {
-  const CustomTextfield({super.key});
+// class CustomTextField extends StatefulWidget {
+//   final TextEditingController controller;
+//    bool? isObscureText;
+//   final Widget? suffixIcon;
+//    CustomTextField({super.key, required this.controller, this.isObscureText, this.suffixIcon});
+//
+//   @override
+//   State<CustomTextField> createState() => _CustomTextFieldState();
+// }
+//
+// class _CustomTextFieldState extends State<CustomTextField> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return TextField(
+//       controller: widget.controller,
+//       obscureText: widget.isObscureText ?? false,
+//       decoration: InputDecoration(
+//         labelText: 'Password',
+//         prefixIcon: const Icon(Icons.lock_outlined),
+//         suffixIcon: widget.suffixIcon,
+//         border: OutlineInputBorder(
+//           borderRadius: BorderRadius.circular(12),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class CustomTextField extends StatelessWidget {
+
+  final TextEditingController controller;
+  final bool isObscureText;
+  final Widget? suffixIcon;
+  final Widget? prefixIcon;
+  final String labelText;
+  final TextInputType? keyboardInputType;
+
+  const CustomTextField(
+      {super.key, required this.controller, required this.isObscureText, this.suffixIcon, this.prefixIcon, required this.labelText, this.keyboardInputType});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextField(
+      controller: controller,
+      obscureText: isObscureText,
+      keyboardType: keyboardInputType,
+      decoration: InputDecoration(
+      labelText: labelText,
+      labelStyle: TextStyle(
+        fontFamily: AppStrings.poppins
+      ),
+
+      prefixIcon: prefixIcon,
+      suffixIcon: suffixIcon,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+    ),);
   }
 }
+

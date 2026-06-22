bed30319c0c2c12293cf375db598b4c61a8c9aba
akshayoffice244 <akshayoffice244@gmail.com>
2026-06-22 13:09:03 +0530

    firebase_service implementation started

--- lib/firebase/firebase_service.dart
+++ lib/firebase/firebase_service.dart
@@ -1,6 +1,34 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ripple/firebase/auth_service.dart';
+import 'package:ripple/models/user_model.dart';
 
 class FirebaseService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final AuthService _authService = AuthService();
 
+  void someFunction() {
+    String? userId = _authService.firebaseAuth.currentUser?.uid;
+  }
+
+  //get user id
+  String? get userUid => _authService.firebaseAuth.currentUser?.uid;
+
+  //users collection reference
+  CollectionReference get usersRef => _firestore.collection("users");
+
+  //current user collection reference
+  DocumentReference get currentUserRef =>
+      _firestore.collection("users").doc(userUid ?? "");
+
+
+  //Chats collection reference
+  CollectionReference get chatsReference => _firestore.collection("chats");
+
+  Stream<List<UserModel>> getAllUsers() {
+    return usersRef.snapshots().map((snapshots) {
+      return snapshots.docs
+          .map((doc) => UserModel.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+    });
+  }
 }
